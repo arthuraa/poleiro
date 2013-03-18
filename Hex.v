00cc2d235776bab5a9125d81f257f0e983fd6f4b
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2013-03-18 18:22:45 -0400

    Finish first draft of post.

--- Hex.v
+++ Hex.v
@@ -12,13 +12,13 @@ Require Import Omega.
 Print string.
 
 (** As we can see, [string]s are much like the [list] type, but
-    contain [ascii] elements instead of an arbitrary type. *)
+    contain [ascii] elements instead of elements of an arbitrary
+    type. [ascii]s, on the other hand, are just eightuples of
+    [bool]s. *)
 
 Print ascii.
 
-(** [ascii]s, on the other hand, are just eightuples of [bool]s.
-
-    Sure enough, if we had to use constructors explicitly for building
+(** Sure enough, if we had to use constructors explicitly for building
     [string]s, using them in Coq wouldn't be much
     practical. Fortunately, Coq provides a convenient notation for
     [string]s and [ascii], much like the built-in notation for
@@ -63,9 +63,11 @@ Definition digitToNat (c : ascii) : option nat :=
     efficient, we can add an [acc] parameter to store the intermediate
     results of the computation -- i.e., the number we've read so far. *)
 
+Open Scope string_scope.
+
 Fixpoint readNatAux (s : string) (acc : nat) : option nat :=
   match s with
-    | EmptyString => Some acc
+    | "" => Some acc
     | String c s' =>
       match digitToNat c with
         | Some n => readNatAux s' (10 * acc + n)
@@ -75,6 +77,9 @@ Fixpoint readNatAux (s : string) (acc : nat) : option nat :=
 
 Definition readNat (s : string) : option nat :=
   readNatAux s 0.
+
+(** We can write some unit tests to make sure our function behaves as
+    expected. *)
 
 Example readNat1 : readNat "1234" = Some 1234.
 Proof. reflexivity. Qed.
@@ -86,6 +91,8 @@ Proof. reflexivity. Qed.
     able to write one for printing them. Again, as a first step, let's
     write a function that converts [nat]s to their corresponding
     digits. *)
+
+Open Scope char_scope.
 
 Definition natToDigit (n : nat) : ascii :=
   match n with
@@ -128,7 +135,8 @@ Proof.
 
 Qed.
 
-(** In particular, we get the following consequence: *)
+(** In particular, we get the following consequence, which will be
+    particularly useful later: *)
 
 Theorem digitToNatNatToDigitMod : forall n : nat,
   digitToNat (natToDigit (n mod 10)) = Some (n mod 10).
@@ -138,15 +146,32 @@ Proof.
   congruence.
 Qed.
 
-(** The natural approach would be to divide the number by 10, print it
-    recursively, and then append the remainder of that division at the
-    end.
+(** Now that we can convert numbers to digits, let's see how we could
+    write our printing function. One idea would be to add a parameter
+    to our function to accumulate the paritially printed number:
 
-    Unfortunately, we can't translate this idea directly as a Coq
-    fixpoint, since we wouldn't be calling the function recursively on
-    a subterm of the original argument. There are several tricks *)
+[[
+    Fixpoint writeNatAux (n : nat) (acc : string) : string :=
+      let acc' := String (natToDigit (n mod 10)) acc in
+      match n / 10 with
+        | 0 => acc'
+        | n' => writeNatAux n' acc'
+      end.
+]]
 
-Open Scope string_scope.
+    The algorithm is straightforward. We print the least signigicant
+    digit of the number, adding it to the string we've printed so far
+    (recall that the [String] constructor adds a character to the
+    front of a [string]). Then, we divide the number by 10 and print
+    it recursively, until we reach zero.
+
+    Unfortunately, Coq doesn't accept this definition, since the
+    recursive call is not done on structurally smaller terms. There
+    are lots of ways to make this definition work, such as using the
+    [Program] command. We will use the somewhat simpler, but standard,
+    trick of adding an explicit "timeout" parameter to our
+    function. *)
+
 
 Fixpoint writeNatAux (time n : nat) (acc : string) : string :=
   let acc' := String (natToDigit (n mod 10)) acc in
@@ -159,8 +184,23 @@ Fixpoint writeNatAux (time n : nat) (acc : string) : string :=
       end
   end.
 
+(** At each step, we do a pattern match on the timeout parameter. If
+    we reach 0, our computation must stop, and we just give back the
+    [string] we've printed so far. Otherwise, we have a structurally
+    smaller argument we can use to do a recursive call, which will
+    make Coq accept the definition.
+
+    Now, in order to effectively print a number, we need to find a
+    timeout value that is big enough to perform the entire
+    computation. In this case, as it turns out, we can use the very
+    the number we're printing for the timeout parameter. *)
+
+Open Scope string_scope.
+
 Definition writeNat (n : nat) : string :=
   writeNatAux n n "".
+
+(** Let's test our definition on some simple examples. *)
 
 Example writeNat1 : writeNat 12 = "12".
 Proof. reflexivity. Qed.
@@ -168,37 +208,99 @@ Proof. reflexivity. Qed.
 Example writeNat2 : writeNat 0 = "0".
 Proof. reflexivity. Qed.
 
+(** It seems clear that [readNat] is indeed the inverse of [writeNat],
+    but can we prove this rigorously in Coq? Formally, we can state
+    the theorem we want as *)
 
-Lemma div_10_le : forall n m,
-  n <= S m -> n / 10 <= m.
-Proof.
-  intros [|n] m H. simpl. omega.
-  assert (S n / 10 < S n); try omega.
-  apply Nat.div_lt; omega.
-Qed.
+Definition readNatWriteNatStatement :=
+  forall n, readNat (writeNat n) = Some n.
 
-Theorem readNatAuxWriteNatAux :
+(** Since both functions are defined in terms of auxiliary fixpoints,
+    it is better to prove something about them first and then use that
+    fact to get the result we want as a consequence. Here's one such
+    possibility: *)
+
+Definition readNatAuxWriteNatAuxStatement :=
   forall time n acc,
     n <= time ->
     readNatAux (writeNatAux time n acc) 0 =
     readNatAux acc n.
+
+(** Notice the precondition [n <= time] in the above statement. As
+    mentioned before, we must give enough time to [writeNatAux] for it
+    to function correctly. This precondition is just stating this fact
+    explicitly.
+
+    It is easy to prove that this lemma will suffice. *)
+
+Theorem readNatWriteNat' :
+  readNatAuxWriteNatAuxStatement ->
+  readNatWriteNatStatement.
 Proof.
-  induction time as [|time' IHtime]; intros n acc H.
-  - simpl. inversion H. reflexivity.
-  - apply div_10_le in H.
-    unfold writeNatAux.
-    destruct (n / 10) as [|n'] eqn:En';
-    [|rewrite IHtime; auto];
-    unfold readNatAux;
-    try rewrite digitToNatNatToDigitMod;
-    try erewrite (div_mod n 10) at 2; try omega;
-    try congruence.
+  unfold readNatAuxWriteNatAuxStatement, readNatWriteNatStatement.
+  unfold readNat, writeNat.
+  intros H n.
+  rewrite H; trivial.
 Qed.
 
-Theorem readNatWriteNat :
-  forall n, readNat (writeNat n) = Some n.
+(** Now, we are ready to prove our intermediate lemma. As usual, since
+    [writeNatAux] is defined by recursion on [time], trying the proof
+    by induction on [time] seems a good guess. Let's begin with a
+    simple lemma that will be useful later. It states that if the
+    precondition for our lemma is satisfied, then it'll also be
+    satisfied for the recursive call, which will allow us to apply the
+    induction hypothesis. *)
+
+Lemma div_10_le : forall n time,
+  n <= S time -> n / 10 <= time.
 Proof.
-  intros [|n]. reflexivity.
-  unfold writeNat, readNat.
-  rewrite readNatAuxWriteNatAux; trivial.
+  intros [|n] time H. simpl. omega.
+  assert (S n / 10 < S n); try omega.
+  apply Nat.div_lt; omega.
+Qed.
+
+(** We can proceed with our proof. *)
+
+Lemma readNatAuxWriteNatAux : readNatAuxWriteNatAuxStatement.
+Proof.
+
+  induction time as [|time' IHtime]; intros n acc H.
+
+  (* The base case is where the inductive hypothesis is crucial, since it
+     forces n to be 0. *)
+
+  - simpl. inversion H. reflexivity.
+
+  (* The inductive case is slightly trickier, but not too hard. We
+     start by applying our previous lemma. *)
+
+  - apply div_10_le in H. unfold writeNatAux.
+
+    (* To simplify the goal, we need to do some case analysis on (n /
+       10). *)
+
+    destruct (n / 10) as [|n'] eqn:En';
+
+    (* We do a recursive call in the non-zero case, and that's where we will
+       need our induction hypothesis. *)
+
+    [|rewrite IHtime; auto]; unfold readNatAux;
+
+    (* Finally, we need the digitToNatNatToDigitMod lemma to show that
+       readNatAux doesn't fail, and the div_mod theorem (defined in the
+       standard library) to express n in terms of (n / 10) and (n mod 10).
+       omega and congruence can easily take care of the end. *)
+
+    try rewrite digitToNatNatToDigitMod;
+    try rewrite (div_mod n 10) at 2; try omega;
+    congruence.
+
+Qed.
+
+(** The final result follows easily. *)
+
+Theorem readNatWriteNat : readNatWriteNatStatement.
+Proof.
+  apply readNatWriteNat'.
+  apply readNatAuxWriteNatAux.
 Qed.
