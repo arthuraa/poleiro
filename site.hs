575de3f4afb3e03b0dc919813a19cbc0b39b3429
Arthur Azevedo de Amorim <arthur@kakapo.(none)>
2013-03-29 21:12:28 -0400

    Use title slug for filename.

--- site.hs
+++ site.hs
@@ -5,10 +5,23 @@ import           Data.Monoid         (mappend, mconcat)
 import qualified Data.Map            as M
 import           Data.Time.Format    (parseTime)
 import           Data.List           (stripPrefix)
+import           Data.Char           (toLower, isAlphaNum)
 import           Hakyll
 import           System.Process
 import           System.FilePath     (takeBaseName)
 import           Text.Regex
+
+makeSlug :: String -> String
+makeSlug = filter (\c -> isAlphaNum c || c == '-') . map subst
+  where subst '_' = '-'
+        subst ' ' = '-'
+        subst c = toLower c
+
+coqPostName :: Metadata -> String
+coqPostName meta =
+  M.findWithDefault "" "date" meta ++ "-"
+  ++ (makeSlug $ M.findWithDefault "" "title" meta)
+  ++ ".html"
 
 compass :: Compiler (Item String)
 compass =
@@ -45,7 +58,7 @@ main = hakyll $ do
         compile copyFileCompiler
 
     match "posts/*.v" $ do
-        route $ setExtension "html"
+        route $ metadataRoute $ constRoute . coqPostName
         compile $ coqdoc >>=
           saveSnapshot "content" >>=
           loadAndApplyTemplate "templates/post.html" postCtx >>=
