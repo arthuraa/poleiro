4a8821d173b34b2ac49fe1a2fa4a1be580dac1c1
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2013-04-13 12:12:09 -0400

    More text on Printf, polishing.

--- posts/Printf.v
+++ posts/Printf.v
@@ -4,7 +4,9 @@ Require Import Coq.Strings.Ascii.
 Require Import Coq.Numbers.Natural.Peano.NPeano.
 
 Notation "[ ]" := nil : list_scope.
-Notation "x :: y" := (String x y) : string_scope.
+Notation "[ x , .. , y ]" := (cons x .. (cons y nil) ..) : list_scope.
+Notation "x ::: y" := (String x y)
+                        (at level 60, right associativity) : string_scope.
 
 Open Scope list_scope.
 Open Scope char_scope.
@@ -19,10 +21,10 @@ Open Scope char_scope.
 
     In spite of being so popular, adding a [printf]-like function may
     be significantly challenging. The number and types of arguments
-    expected by [printf] depends on the format parameter passed to it,
-    something that is incompatible with most static type systems. In
+    expected by [printf] depend on the format parameter passed to it,
+    something that is incompatible with many static type systems. In
     C, the problem is solved by allowing funcitons to take a variable
-    number of arguments, but there is no way of the type system to
+    number of arguments, but there is no way for the type system to
     ensure that the number and types of those arguments match the
     specification given in the format string. OCaml has its own
     version of [printf], which does track the dependencies between the
@@ -35,13 +37,22 @@ Open Scope char_scope.
 
 (** ** Directives and format
 
-    We will begin our implementation by expressing what a [printf]
-    format is. In C, a [printf] format is just a string, which makes
-    writing them very easy. As a crude approximation to the C
-    interface, a [printf] format is just a string with a sequence of
-    directives marked by [%] characters. We will model literal strings
-    and directives as a separate data type, and a format will be just
-    a sequence of directives. *)
+    The power of [printf] comes from how succint strings are for
+    writing output formats. The format argument looks very similar to
+    the final output that we expect, which makes calls to the function
+    very easy to understand.
+
+    Even though we want to use Coq [string]s for the format argument,
+    it is more convenient to first implement [printf] using a separate
+    [format] type. In the final implementation, we will parse the
+    [string] argument as a [format] and pass it to that function.
+
+    In C, a [printf] format is a sequence of _directives_. Each
+    directive can be either a literal character, which will be printed
+    verbatim, or a _control sequence_, which instructs the function to
+    print one of its arguments in a certain format. For our version of
+    [printf] we will consider a small set of control sequences, based
+    roughly on the ones available in C. *)
 
 Inductive directive : Type :=
 | DLit : ascii -> directive
@@ -52,17 +63,17 @@ Inductive directive : Type :=
 
 Definition format := list directive.
 
-(** Constructors [DBool], [DString] and [DChar] just signal that we
-    will output an element of the corresponding type. [DLiteral c]
-    just prints the constant character [c]. Finally, [DNum s] is used
-    to output numbers (i.e., Coq [nat]s) according to the [s]
-    parameter. If [s = Some n], then we output exactly the [n]
-    least-significant digits of the number, padding it with zeros if
-    necessary. Otherwise, we just print the whole number.
+(** Directive [DLit c] corresponds to the literal character
+    [c]. [DBool], [DString] and [DChar] signal that we will output an
+    element of the corresponding type.  More interestingly, [DNum s]
+    is used to output numbers (i.e., Coq [nat]s). The [s] parameter
+    controls the length of the number we will print. If [s = Some n],
+    then we output exactly the [n] least-significant digits of the
+    number, padding it with zeros if necessary. Otherwise, if [s =
+    None], we just print the whole number.
 
-    Given that definition, we can write a Coq function that takes a
-    [format] (i.e., a sequence of directives) and computes the
-    corresponding Coq type. *)
+    Given a format [f], we can compute the type that [printf f] should
+    have as follows: *)
 
 Fixpoint formatType (f : format) : Type :=
   match f with
@@ -77,14 +88,39 @@ Fixpoint formatType (f : format) : Type :=
       end
   end%list.
 
-(** All directives add an additional argument to the type, except for
-    [DLit], whose behavior is already fully determined by its
-    immediate argument. *)
+(** Notice that all directives except [DLit] add an argument to the
+    type.
 
-(** ** A first implementation
+    ** The implementation
 
-    Now that we can express the type for a [format], we can code a
-    first implementation of [printf]. *)
+    Now that we can express the type for a [format], we can try to
+    implement [printf]. As a first attempt, we might try something
+    like this:
+
+[[
+Fixpoint printfImpl (f : format) : formatType f :=
+  match f with
+    | [] => ""
+    | dir :: dirs =>
+      match dir with
+        | DLit c => c :: printfImpl dirs
+        (* ... *)
+      end
+  end.
+]]
+
+    Alas, this approach doesn't work. The problem here is that our
+    recursive call to [printfImpl] returns a [formatType dirs] instead
+    of a [string], which means that we are unable to add character [c]
+    in front of it.
+
+    Instead of building the string directly on the body of the match,
+    we will add an auxiliary parameter [k] to [printfImpl]. To make it
+    more efficient, this parameter will be a _continuation_ of type
+    [string -> string] that builds the final output from the result of
+    the recursive calls. For brevity, I've omitted the definition of
+    some auxiliary functions, such as [writeNat], but they can be
+    found in the original [.v] file. *)
 
 (* begin hide *)
 Definition digitToNat (c : ascii) : option nat :=
@@ -122,7 +158,7 @@ Fixpoint writeNatAux (time n : nat) (crop : bool) (acc : string) : string :=
   match time with
     | 0 => acc
     | S time' =>
-      let acc' := natToDigit (n mod 10) :: acc in
+      let acc' := natToDigit (n mod 10) ::: acc in
       match n / 10 with
         | 0 => if crop then acc'
                else writeNatAux time' 0 crop acc'
@@ -130,79 +166,119 @@ Fixpoint writeNatAux (time n : nat) (crop : bool) (acc : string) : string :=
       end
   end.
 
-Definition writeNat (n : nat) : string :=
-  writeNatAux (S n) n true "".
+Definition writeNat (n : nat) (acc : string) : string :=
+  writeNatAux (S n) n true acc.
 
-Definition writeNatSize (size : nat) (n : nat) : string :=
-  writeNatAux size n false "".
+Definition writeNatSize (size : nat) (n : nat) (acc : string) : string :=
+  writeNatAux size n false acc.
 
 Definition writeBool (b : bool) : string :=
   if b then "true" else "false".
 (* end hide *)
 
-Fixpoint printfImpl (f : format) (acc : string) : formatType f :=
+Fixpoint printfImpl (f : format) (k : string -> string) : formatType f :=
   match f with
-    | [] => acc
-    | (dir :: dirs)%list =>
-      match dir return formatType (dir :: dirs)%list with
-        | DLit c => printfImpl dirs (acc ++ c :: "")
-        | DNum o => fun n =>
-                      printfImpl dirs
-                                 match o with
-                                   | Some size => acc ++ writeNatSize size n
-                                   | None => acc ++ writeNat n
-                                 end
-        | DBool => fun b => printfImpl dirs (acc ++ writeBool b)
-        | DString => fun s => printfImpl dirs (acc ++ s)
-        | DChar => fun c => printfImpl dirs (acc ++ c :: "")
+    | [] => k ""
+    | dir :: dirs =>
+      match dir return formatType (dir :: dirs) with
+        | DLit c => printfImpl dirs (fun res => k (c ::: res))
+        | DNum o =>
+          fun n =>
+            let k' := match o with
+                        | Some size => fun res => k (writeNatSize size n res)
+                        | None => fun res => k (writeNat n res)
+                      end in
+            printfImpl dirs k'
+        | DBool =>
+          fun b =>
+            printfImpl dirs (fun res => k (writeBool b ++ res))
+        | DString =>
+          fun s =>
+            printfImpl dirs (fun res => k (s ++ res))
+        | DChar =>
+          fun c =>
+            printfImpl dirs (fun res => k (c ::: res))
       end
   end.
 
-(** The first argument [f] is just the format that we are using,
-    whereas the extra [acc] argument is just an auxiliary variable to
-    store the [string] produced so far. The result of the function,
-    [formatType f], ensures that the function will be able to take the
-    arguments we want it to depending on which format was passed to
-    it.
+(** Our implementation mimics the definition of [formatType], to
+    ensure that the types will match accordingly. Notice that it is
+    still necessary to annotate the return type of the second [match]
+    explicitly, because Coq is not able to infer it.
 
-    Notice how the structure of the pattern-matching is very close to
-    the one used to define [formatType]. This is done to make it
-    easier for Coq to infer and check the types. Unfortunately,
-    inference is not powerful enough to figure out everything on its
-    own, so we need to add a type annotation for the second
-    pattern-matching. *)
+    To use [printfImpl], we just have to pass it the identity
+    continuation [fun res => res], that will receive the value built
+    by [printfImpl] and return it as-is. *)
+
+Example printfImpl1 :
+  printfImpl [DNum None, DString] (fun res => res)
+             42 "This is a string" = "42This is a string".
+Proof. reflexivity. Qed.
+
+Example printfImpl2 :
+  printfImpl [DNum (Some 2), DLit "/", DNum (Some 2)] (fun res => res)
+             2 4 = "02/04".
+Proof. reflexivity. Qed.
+
+(** ** Strings as format
+
+    Now that we have our first implementation, we will write the code
+    needed to parse the format from a [string]. Our format syntax is
+    inspired by C's own syntax. All characters are interpreted
+    literally, except for [%], which signals the beginning of a
+    control sequence. As in C, we can write [%<n>d], where [<n>] is a
+    number, to specify how many digits we want when printing a [nat].
+
+    The [parseFormat] function below tries to read a [format],
+    returning [Some f] if the [string] argument represents [f], and
+    [None] if there was a parse error. [parseFormatSize] is used to
+    read the [%<n>d] directives. *)
 
 Definition consOpt {A} (x : A) (o : option (list A)) : option (list A) :=
   match o with
-    | Some xs => Some (x :: xs)%list
+    | Some xs => Some (x :: xs)
     | None => None
   end.
 
 Fixpoint parseFormat (s : string) : option format :=
   match s with
     | "" => Some []
-    | "%" :: s' =>
+    | "%" ::: s' =>
       match s' with
-        | "%" :: s'' => consOpt (DLit "%"%char) (parseFormat s'')
-        | "b" :: s'' => consOpt DBool (parseFormat s'')
-        | "s" :: s'' => consOpt DString (parseFormat s'')
-        | "c" :: s'' => consOpt DChar (parseFormat s'')
+        | "%" ::: s'' => consOpt (DLit "%"%char) (parseFormat s'')
+        | "b" ::: s'' => consOpt DBool (parseFormat s'')
+        | "s" ::: s'' => consOpt DString (parseFormat s'')
+        | "c" ::: s'' => consOpt DChar (parseFormat s'')
+        | "d" ::: s'' => consOpt (DNum None) (parseFormat s'')
         | _ => parseFormatSize s' 0
       end
-    | c :: s' =>
+    | c ::: s' =>
       consOpt (DLit c) (parseFormat s')
   end
 
 with parseFormatSize (s : string) (acc : nat) : option format :=
        match s with
          | "" => None
-         | "d" :: s' => consOpt (DNum (Some acc)) (parseFormat s')
-         | c :: s' =>
+         | "d" ::: s' => consOpt (DNum (Some acc)) (parseFormat s')
+         | c ::: s' =>
            match digitToNat c with
              | Some n => parseFormatSize s' (10 * acc + n)
              | None => None
            end
        end.
+
+(** We can test our function in some simple cases. *)
+
+Example parseFormat1 :
+  parseFormat "%d%4da" = Some [DNum None, DNum (Some 4), DLit "a"].
+Proof. reflexivity. Qed.
+
+(** ** Putting the pieces together
+
+    Using [parseFormat], we can write a convenient wrapper for
+    [printfImpl]. We can use the same trick as
+    #<a href="/posts/2013-04-03-parse-errors-as-type-errors.html">before</a># to
+    ensure that invalid format strings will be noticed right away. *)
 
 Inductive printfError := InvalidFormat.
 
@@ -219,8 +295,8 @@ Definition printf (s : string) : printfT s :=
                                | None => printfError
                              end
   with
-    | Some f => printfImpl f ""
+    | Some f => printfImpl f (fun res => res)
     | None => InvalidFormat
   end.
 
-Eval compute in printf "Hello, I'm %s and I am %2d years old" "Arthur" 2.
+Eval vm_compute in printf "Hello, I'm %s and I am %2d years old" "Arthur" 2.
