f3d8834a8d7144ede3f9c90f0c2929c3f6a6370f
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2013-10-26 16:47:22 -0400

    Define post content.

--- theories/CGT2.v
+++ theories/CGT2.v
@@ -4,28 +4,6 @@ Require Import Coq.Wellfounded.Lexicographic_Product.
 Require Import Coq.Relations.Relation_Operators.
 Require Import CGT.
 
-Fixpoint negate (g : game) : game :=
-  match g with
-    | Game l r =>
-      Game (map negate r) (map negate l)
-  end.
-
-Lemma negate_involutive :
-  forall g, negate (negate g) = g.
-Proof.
-  induction g as [l r IHl IHr] using game_ind'.
-  simpl.
-  f_equal.
-  - clear IHr.
-    induction IHl as [|g gs Hg _ IH]; simpl; eauto.
-    rewrite Hg, IH.
-    reflexivity.
-  - clear IHl.
-    induction IHr as [|g gs Hg _ IH]; simpl; eauto.
-    rewrite Hg, IH.
-    reflexivity.
-Qed.
-
 Fail Fixpoint sum (g1 g2 : game) : game :=
   match g1, g2 with
     | Game l1 r1, Game l2 r2 =>
@@ -33,223 +11,112 @@ Fail Fixpoint sum (g1 g2 : game) : game :=
            (map (fun g1 => sum g1 g2) r1 ++ map (fun g2 => sum g1 g2) r2)
   end.
 
-Definition game_pair_order :=
-  symprod _ _ (valid_move game_as_cg) (valid_move game_as_cg).
-Definition game_pair_order_wf : well_founded game_pair_order :=
-  wf_symprod _ _ _ _ (finite_game game_as_cg) (finite_game game_as_cg).
-
-Definition sum_body (gs : game * game) :=
-  match gs return (forall gs', game_pair_order gs' gs -> game) -> game with
-    | (Game l1 r1, Game l2 r2) =>
-      fun F =>
-        {| left_moves :=
-             map_In l1
-                    (fun g1 p => F (g1, Game l2 r2)
-                                   (left_sym game game
-                                             (valid_move game_as_cg)
-                                             (valid_move game_as_cg)
-                                             g1 (Game l1 r1) (ex_intro _ Left p)
-                                             (Game l2 r2))) ++
-             map_In l2
-                    (fun g2 p => F (Game l1 r1, g2)
-                                   (right_sym game game
-                                              (valid_move game_as_cg)
-                                              (valid_move game_as_cg)
-                                              g2 (Game l2 r2) (ex_intro _ Left p)
-                                              (Game l1 r1)));
-           right_moves :=
-             map_In r1
-                    (fun g1 p => F (g1, Game l2 r2)
-                                   (left_sym game game
-                                             (valid_move game_as_cg)
-                                             (valid_move game_as_cg)
-                                             g1 (Game l1 r1) (ex_intro _ Right p)
-                                             (Game l2 r2))) ++
-             map_In r2
-                    (fun g2 p => F (Game l1 r1, g2)
-                                   (right_sym game game
-                                              (valid_move game_as_cg)
-                                              (valid_move game_as_cg)
-                                              g2 (Game l2 r2) (ex_intro _ Right p)
-                                              (Game l1 r1))) |}
-  end.
+Definition game_pair_order {cg1 cg2 : combinatorial_game} :=
+  symprod _ _ (valid_move cg1) (valid_move cg2).
+Definition gpo_introl {cg1 cg2}
+                      (g1 g1' : position cg1)
+                      (P : valid_move cg1 g1' g1)
+                      (g2 : position cg2)
+                      : game_pair_order (g1', g2) (g1, g2) :=
+  left_sym _ _ _ _ g1' g1 P g2.
+Definition gpo_intror {cg1 cg2 : combinatorial_game}
+                      (g2 g2' : position cg2)
+                      (P : valid_move cg2 g2' g2)
+                      (g1 : position cg1)
+                      : game_pair_order (g1, g2') (g1, g2) :=
+  right_sym _ _ _ _ g2' g2 P g1.
+Definition game_pair_order_wf cg1 cg2 : well_founded game_pair_order :=
+  wf_symprod _ _ _ _ (finite_game cg1) (finite_game cg2).
 
 Definition sum (g1 g2 : game) : game :=
-  Fix game_pair_order_wf (fun _ => game)
-      sum_body (g1, g2).
+  Fix (game_pair_order_wf game_as_cg game_as_cg) (fun _ => game)
+      (fun gs =>
+         match gs with
+           | (g1, g2) =>
+             fun F =>
+               Game (map_game game_as_cg g1 Left
+                              (fun g1' P => F _ (gpo_introl g1 g1' P g2)) ++
+                     map_game game_as_cg g2 Left
+                              (fun g2' P => F _ (gpo_intror g2 g2' P g1)))
+                    (map_game game_as_cg g1 Right
+                              (fun g1' P => F _ (gpo_introl g1 g1' P g2)) ++
+                     map_game game_as_cg g2 Right
+                              (fun g2' P => F _ (gpo_intror g2 g2' P g1)))
+         end) (g1, g2).
 
+Lemma sum_eq (g1 g2 : game) :
+  sum g1 g2 =
+  Game (map (fun g1' => sum g1' g2) (left_moves g1) ++
+        map (fun g2' => sum g1 g2') (left_moves g2))
+       (map (fun g1' => sum g1' g2) (right_moves g1) ++
+        map (fun g2' => sum g1 g2') (right_moves g2)).
+Proof.
+  unfold sum at 1.
+  simpl.
+  rewrite Fix_eq.
+  - intros.
+    do 2 f_equal; apply map_game_map; reflexivity.
+  - clear. intros [g1 g2] f g EXT.
+    do 2 f_equal; apply map_game_ext; eauto.
+Qed.
 
-
-
-
-(* Let's define a more interesting function on games: sum. As we've
-seen, Coq rejects the direct definition of sum, because it's not
-structurally recursive on a single argument. We use the trick here of
-passing an additional parameter to sum. *)
-
-Fixpoint height (g : game) :=
-  S (max (fold_left max (map height (left_moves g)) 0)
-         (fold_left max (map height (right_moves g)) 0)).
-
-Fixpoint sum_aux (max_height : nat) (g1 g2 : game) :=
-  match max_height with
-    | 0 =>
-      (* We'll never reach 0 normally, so we just return some arbitrary value here *)
-      zero
-    | S n =>
-      match g1, g2 with
-        | Game l1 r1, Game l2 r2 =>
-          Game (map (fun g1 => sum_aux n g1 g2) l1 ++
-                map (fun g2 => sum_aux n g1 g2) l2)
-               (map (fun g1 => sum_aux n g1 g2) r1 ++
-                map (fun g2 => sum_aux n g1 g2) r2)
-      end
+Program Definition sum_cg (cg1 cg2 : combinatorial_game)
+                          : combinatorial_game :=
+  {| position := (position cg1 * position cg2);
+     moves p pos := map (fun pos1' => (pos1', snd pos))
+                        (moves cg1 p (fst pos)) ++
+                    map (fun pos2' => (fst pos, pos2'))
+                        (moves cg2 p (snd pos)) |}.
+Next Obligation.
+  match goal with
+    | |- well_founded ?R =>
+      assert (EQ : RelationClasses.relation_equivalence R game_pair_order)
   end.
+  { intros [pos1' pos2'] [pos1 pos2]. split.
+    - intros [p H].
+      apply in_app_or in H.
+      repeat rewrite in_map_iff in H.
+      destruct H as [(pos1'' & H1 & H2) | (pos2'' & H1 & H2)];
+      simpl in *; inversion H1; subst; clear H1;
+      constructor (solve [eexists; eauto]).
+    - intros H.
+      inversion H as [? ? [p H'] ?|? ? [p H'] ?]; subst; clear H;
+      exists p; rewrite in_app_iff; repeat rewrite in_map_iff;
+      simpl; eauto. }
+  rewrite EQ.
+  apply game_pair_order_wf.
+Qed.
 
-Definition sum (g1 g2 : game) : game :=
-  sum_aux (max (height g1) (height g2)) g1 g2.
-
-Lemma sum_aux_
-
-Lemma sum_eq :
-  forall g1 g2,
-    sum g1 g2 =
-    Game (map (fun g1 => sum g1 g2) (left_moves g1) ++
-          map (fun g2 => sum g1 g2) (left_moves g2))
-         (map (fun g1 => sum g1 g2) (right_moves g1) ++
-          map (fun g2 => sum g1 g2) (right_moves g2)).
+Lemma map_ext_strong :
+  forall {A B}
+         (l : list A)
+         (f g : A -> B)
+         (EXT : forall x, In x l -> f x = g x),
+    map f l = map g l.
 Proof.
   intros.
-  unfold sum.
-  generalize dependent
+  induction l as [|x l IH]; trivial.
+  simpl.
+  f_equal.
+  - apply EXT. simpl. auto.
+  - apply IH. intros. apply EXT. simpl. auto.
+Qed.
 
-
-(* Using sum, we can define minus *)
-
-Definition minus (g1 g2 : game) : game :=
-  sum g1 (negate g2).
-
-(* Now let's analyze the winning profile of games. We begin by
-defining two (mutually recursive) functions that return true if left
-can win if it plays first (resp. if it plays second) *)
-
-Fixpoint left_wins_first (g : game) : bool :=
-  existsb left_wins_second (left_moves g)
-with left_wins_second (g : game) : bool :=
-  forallb left_wins_first (right_moves g).
-
-(* Defining if left always wins now is easy. *)
-
-Definition left_wins (g : game) : bool :=
-  match g with
-    | Game l r =>
-      existsb left_wins_second l &&
-      forallb left_wins_first r
-  end.
-
-(* We can use the previous functions and negate to define similar
-functions for the right player. *)
-
-Definition right_wins_first (g : game) : bool :=
-  left_wins_first (negate g).
-Definition right_wins_second (g : game) : bool :=
-  left_wins_second (negate g).
-Definition right_wins (g : game) : bool :=
-  left_wins (negate g).
-
-(* Finally, two functions that return true iff the first (resp. the
-second) player to play always wins *)
-
-Fixpoint first_wins (g : game) : bool :=
-  match g with
-    | Game l r =>
-      existsb left_wins_second l &&
-      existsb left_wins_second r
-  end.
-
-Definition second_wins (g : game) : bool :=
-  match g with
-    | Game l r =>
-      forallb right_wins_first l &&
-      forallb left_wins_first r
-  end.
-
-(* We can check that these functions behave as expected on some arguments: *)
-
-Lemma left_wins_test_1 : left_wins zero = false.
-Proof. reflexivity. Qed.
-Lemma left_wins_test_2 : left_wins one = true.
-Proof. reflexivity. Qed.
-Lemma left_wins_test_3 : left_wins two = true.
-Proof. reflexivity. Qed.
-
-Lemma right_wins_test_1 : right_wins zero = false.
-Proof. reflexivity. Qed.
-Lemma right_wins_test_2 : right_wins one = false.
-Proof. reflexivity. Qed.
-Lemma right_wins_test_3 : right_wins minus_one = true.
-Proof. reflexivity. Qed.
-
-Lemma first_wins_test_1 : first_wins zero = false.
-Proof. reflexivity. Qed.
-Lemma first_wins_test_2 : first_wins one = false.
-Proof. reflexivity. Qed.
-Lemma first_wins_test_3 : first_wins star = true.
-Proof. reflexivity. Qed.
-
-Lemma second_wins_test_1 : second_wins zero = true.
-Proof. reflexivity. Qed.
-Lemma second_wins_test_2 : second_wins one = false.
-Proof. reflexivity. Qed.
-Lemma second_wins_test_3 : second_wins star = false.
-Proof. reflexivity. Qed.
-
-(* Using those, we can finally define comparison functions between games. *)
-
-Definition gt (g1 g2 : game) : bool := left_wins (minus g1 g2).
-Definition lt (g1 g2 : game) : bool := right_wins (minus g1 g2).
-Definition eq (g1 g2 : game) : bool := second_wins (minus g1 g2).
-Definition incomp (g1 g2 : game) : bool := first_wins (minus g1 g2).
-
-Lemma gt_test : gt one zero = true.
-Proof. reflexivity. Qed.
-Lemma lt_test : lt minus_one zero = true.
-Proof. reflexivity. Qed.
-Lemma eq_test : eq (sum star star) zero = true.
-Proof. reflexivity. Qed.
-Lemma incomp_test : incomp star zero = true.
-Proof. reflexivity. Qed.
-
-(* In case you're thinking this is too gross, you can abstract things
-a little bit more. Let's define a datatype for representing players,
-and a function for comparing members of that type. *)
-
-
-Definition side_eq (s1 s2 : side) : bool :=
-  match s1, s2 with
-    | Left, Left => true
-    | Right, Right => true
-    | _, _ => false
-  end.
-
-
-(* And moves selects the moves of a player from a game *)
-
-Definition moves (g : game) (s : side) :=
-  match s with
-    | Left => left_moves g
-    | Right => right_moves g
-  end.
-
-(* We can now write a function [wins s first g] that returns true iff
-player [s] always wins game [g] when player [first] begins playing. *)
-
-Fixpoint wins (s first : side) (g : game) : bool :=
-  if side_eq s first then
-    existsb (wins s (other first)) (moves g first)
-  else
-    forallb (wins s (other first)) (moves g first).
-
-(* Now, we can define left_wins and right_wins generically: *)
-
-Definition always_wins s g := wins s s g && wins s (other s) g.
+Lemma sum_is_sum (cg1 cg2 : combinatorial_game)
+                 (pos1 : position cg1) (pos2 : position cg2) :
+  embed_in_game (sum_cg cg1 cg2) (pos1, pos2) =
+  sum (embed_in_game cg1 pos1) (embed_in_game cg2 pos2).
+Proof.
+  remember (pos1, pos2) as pos.
+  replace pos1 with (fst pos) by (destruct pos; simpl; congruence).
+  replace pos2 with (snd pos) by (destruct pos; simpl; congruence).
+  clear.
+  induction pos as [[pos1 pos2] IH]
+                using (well_founded_ind (game_pair_order_wf _ _)).
+  rewrite embed_in_game_eq, sum_eq. simpl.
+  repeat rewrite (embed_in_game_moves _ _ Left).
+  repeat rewrite (embed_in_game_moves _ _ Right).
+  repeat rewrite map_app. repeat rewrite map_map.
+  do 2 f_equal;
+  apply map_ext_strong; intros pos IN; apply IH; constructor; eexists; eauto.
+Qed.
