4af9439db09971da13429ad02c505403d2a2e6ee
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-07-15 17:52:13 +0200

    Improve naming and argument consistency.

--- theories/Eggs.v
+++ theories/Eggs.v
@@ -151,21 +151,21 @@ Fixpoint optimal_range (e t : nat) {struct t} : nat :=
   | _, _ => 0
   end.
 
-Fixpoint optimal_strategy (e t lower : nat) : strategy :=
+Fixpoint optimal (lower e t : nat) {struct t} : strategy :=
   match t, e with
   | S t', S e' =>
     let floor := lower + optimal_range e' t' in
     Drop floor
-         (optimal_strategy e' t' lower)
-         (optimal_strategy (S e') t' (S floor))
+         (optimal lower e' t')
+         (optimal (S floor) (S e') t')
   | _, _ => Guess lower
   end.
 
 (** It is easy to show that [optimal_strategy] indeed uses the
 resources that we expect. *)
 
-Lemma optimal_strategy_winning e t lower :
-  winning lower (S (optimal_range e t)) (optimal_strategy e t lower).
+Lemma optimal_strategy_winning lower e t :
+  winning lower (S (optimal_range e t)) (optimal lower e t).
 Proof.
   generalize dependent lower.
   generalize dependent e.
@@ -178,8 +178,8 @@ Proof.
     + apply IH. apply leb_iff_conv in E. omega.
 Qed.
 
-Lemma optimal_strategy_eggs e t lower :
-  eggs (optimal_strategy e t lower) = min e t.
+Lemma optimal_strategy_eggs lower e t :
+  eggs (optimal lower e t) = min e t.
 Proof.
   generalize dependent lower.
   generalize dependent e.
@@ -189,8 +189,8 @@ Proof.
   destruct t; simpl; lia.
 Qed.
 
-Lemma optimal_strategy_tries e t lower :
-  tries (optimal_strategy e t lower) = min 1 e * t.
+Lemma optimal_strategy_tries lower e t :
+  tries (optimal lower e t) = min 1 e * t.
 Proof.
   generalize dependent lower.
   generalize dependent e.
@@ -204,26 +204,26 @@ Qed.
 indeed computes what it's supposed to. We start by showing two
 inversion lemmas. *)
 
-Lemma winning_inv_guess lower n floor :
-  winning lower n (Guess floor) -> n <= 1.
+Lemma winning_inv_guess lower range floor :
+  winning lower range (Guess floor) -> range <= 1.
 Proof.
   intros WIN.
-  destruct n as [|[|n]]; try omega.
+  destruct range as [|[|range]]; try omega.
   assert (play lower (Guess floor) = true) by (apply WIN; omega).
   assert (play (lower + 1) (Guess floor) = true) by (apply WIN; omega).
   simpl in *. rewrite beq_nat_true_iff in *. omega.
 Qed.
 
-Lemma winning_inv_drop lower n floor broken intact :
-  winning lower n (Drop floor broken intact) ->
-  exists n1 n2 lower',
-    n = n1 + n2 /\
-    winning lower n1 broken /\
-    winning lower' n2 intact.
+Lemma winning_inv_drop lower range floor broken intact :
+  winning lower range (Drop floor broken intact) ->
+  exists r1 r2 lower',
+    range = r1 + r2 /\
+    winning lower r1 broken /\
+    winning lower' r2 intact.
 Proof.
   unfold winning. simpl. intros WIN.
-  destruct (le_lt_dec (lower + n) floor) as [LE | LT].
-  - eexists n, 0, 0.
+  destruct (le_lt_dec (lower + range) floor) as [LE | LT].
+  - eexists range, 0, 0.
     split; try omega.
     split; try solve [intros; omega].
     intros goal I.
@@ -231,16 +231,16 @@ Proof.
     apply WIN in I.
     rewrite <- leb_iff in BOUND. now rewrite BOUND in I.
   - destruct (le_lt_dec lower floor) as [LE' | LT'].
-    + eexists (S floor - lower), (lower + n - S floor), (S floor).
+    + eexists (S floor - lower), (lower + range - S floor), (S floor).
       split; try omega.
       split; intros goal I;
-      assert (I' : lower <= goal < lower + n) by omega;
+      assert (I' : lower <= goal < lower + range) by omega;
       apply WIN in I'.
       * assert (BOUND : goal <= floor) by omega.
         rewrite <- leb_iff in BOUND. now rewrite BOUND in I'.
       * assert (BOUND : floor < goal) by omega.
         rewrite <- leb_iff_conv in BOUND. now rewrite BOUND in I'.
-    + eexists 0, n, lower.
+    + eexists 0, range, lower.
       split; trivial.
       split; intros goal I; try omega.
       assert (BOUND : floor < goal) by omega.
@@ -249,19 +249,22 @@ Proof.
 Qed.
 
 Lemma optimal_range_correct :
-  forall t e s lower n,
-    tries s <= t ->
+  forall lower e t s range,
     eggs s  <= e ->
-    winning lower n s ->
-    n <= S (optimal_range e t).
+    tries s <= t ->
+    winning lower range s ->
+    range <= S (optimal_range e t).
 Proof.
-  induction t as [|t IH]; intros e s lower n Ht He WIN;
+  intros lower e t.
+  generalize dependent e.
+  generalize dependent lower.
+  induction t as [|t IH]; intros lower e s range He Ht WIN;
   destruct s as [floor|floor broken intact]; try solve [inversion Ht].
   - apply winning_inv_guess in WIN. lia.
   - apply winning_inv_guess in WIN.
     destruct e as [|e]; simpl in *; try lia.
   - apply winning_inv_drop in WIN.
-    destruct WIN as (n1 & n2 & lower' & ? & WIN1 & WIN2). subst n.
+    destruct WIN as (r1 & r2 & lower' & ? & WIN1 & WIN2). subst range.
     assert (He' : exists e', e = S e' /\
                              eggs broken <= e' /\
                              eggs intact <= S e').
@@ -274,29 +277,26 @@ Proof.
     { unfold tries in Ht. fold tries in Ht. lia. }
     destruct Ht' as [Htb Hti]. clear Ht.
     simpl.
-    cut (n1 <= S (optimal_range e' t) /\
-         n2 <= S (optimal_range (S e') t)); try lia.
+    cut (r1 <= S (optimal_range e' t) /\
+         r2 <= S (optimal_range (S e') t)); try lia.
     split.
-    + apply (IH e' broken lower n1); try lia.
+    + apply (IH lower e' broken r1); try lia.
       now trivial.
-    + apply (IH (S e') intact lower' n2); try lia.
+    + apply (IH lower' (S e') intact r2); try lia.
       now trivial.
 Qed.
 
-(** Using this lemma, we can derive some useful facts about
-[optimal_range]. *)
-
 Lemma optimal_range_monotone :
-  forall t t' e e',
-    t <= t' ->
+  forall e e' t t',
     e <= e' ->
+    t <= t' ->
     optimal_range e t <= optimal_range e' t'.
 Proof.
-  intros t t' e e' Ht He.
+  intros e e' t t' He Ht.
   cut (S (optimal_range e t) <= S (optimal_range e' t')); try lia.
-  apply (optimal_range_correct t' e' (optimal_strategy e t 0) 0);
-    [ rewrite optimal_strategy_tries; destruct e; simpl; lia
-    | rewrite optimal_strategy_eggs; lia
+  apply (optimal_range_correct 0 e' t' (optimal 0 e t));
+    [ rewrite optimal_strategy_eggs; lia
+    | rewrite optimal_strategy_tries; destruct e; simpl; lia
     | apply optimal_strategy_winning ].
 Qed.
 
@@ -305,9 +305,9 @@ Lemma optimal_range_lower_bound :
 Proof.
   intros e t.
   cut (S t <= S (optimal_range (S e) t)); try lia.
-  apply (optimal_range_correct t (S e) (linear 0 t) 0);
-    [ rewrite linear_tries
-    | rewrite linear_eggs
+  apply (optimal_range_correct 0 (S e) t (linear 0 t));
+    [ rewrite linear_eggs
+    | rewrite linear_tries
     | apply linear_winning ]; lia.
 Qed.
 
@@ -344,19 +344,19 @@ Definition find_optimum e goal :=
   find_root (fun t => S (optimal_range e t)) goal goal.
 
 Lemma find_optimum_correct :
-  forall e goal,
-    let t := find_optimum (S e) goal in
-    is_optimal goal (optimal_strategy (S e) t 0).
+  forall e range,
+    let t := find_optimum (S e) range in
+    is_optimal range (optimal 0 (S e) t).
 Proof.
-  intros e goal t.
-  assert (H : goal <= S (optimal_range (S e) t) /\
-              forall t', t' < t -> S (optimal_range (S e) t') < goal).
+  intros e range t.
+  assert (H : range <= S (optimal_range (S e) t) /\
+              forall t', t' < t -> S (optimal_range (S e) t') < range).
   { subst t.
-    apply (find_root_correct (fun t => S (optimal_range (S e) t)) goal goal).
+    apply (find_root_correct (fun t => S (optimal_range (S e) t)) range range).
     - intros t t' Ht.
       cut (optimal_range (S e) t <= optimal_range (S e) t'); try lia.
       apply optimal_range_monotone; lia.
-    - cut (goal <= optimal_range (S e) goal); try lia.
+    - cut (range <= optimal_range (S e) range); try lia.
       apply optimal_range_lower_bound. }
   destruct H as [H1 H2].
   exists 0. split.
@@ -369,5 +369,6 @@ Proof.
     assert (Ht : tries s <= tries s) by lia.
     assert (He : eggs s <= S e) by lia.
     pose proof (H2 _ LT).
-    pose proof (optimal_range_correct _ _ _ _ _ Ht He WIN). lia.
+    pose proof (optimal_range_correct _ _ _ _ _ He Ht WIN).
+    lia.
 Qed.
