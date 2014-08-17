0dd35a1c66f7dc776b6beba3bc6547596714b848
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-08-17 21:48:56 +0200

    Complete version of theorem.

--- theories/Sorting.v
+++ theories/Sorting.v
@@ -299,19 +299,6 @@ Proof.
     + now rewrite map_length, seq_length.
 Qed.
 
-Fixpoint insert (n : nat) (l : list nat) : list nat :=
-  match l with
-  | [] => [n]
-  | n' :: l' => if leb n n' then n :: n' :: l'
-                else n' :: insert n l'
-  end.
-
-Fixpoint sort (l : list nat) : list nat :=
-  match l with
-  | [] => []
-  | n :: l' => insert n (sort l')
-  end.
-
 Lemma log2_fact_inv n :
   n * log2 n + n * 2 <= log2 (fact n) * 2 + 2 ^ (log2 n) * 2 + 1.
 Proof.
@@ -352,41 +339,46 @@ Proof.
   rewrite (div_mod (n * log2 n) 2) in H; lia.
 Qed.
 
-Inductive sorting_algorithm : Type :=
-| Compare (n m : nat) (l r : sorting_algorithm)
-| Done (p : list nat).
+Inductive sorting_algorithm (s : nat) : Type :=
+| Compare (n m : nat) (l r : sorting_algorithm s)
+| Done (p : nat) (H : p < fact s).
 
-Fixpoint comparisons (s : sorting_algorithm) : nat :=
-  match s with
+Fixpoint comparisons s (alg : sorting_algorithm s) : nat :=
+  match alg with
   | Compare _ _ l r => S (max (comparisons l) (comparisons r))
-  | Done _ => 0
+  | Done _ _ => 0
   end.
 
-Fixpoint execute (alg : sorting_algorithm) (xs : list nat) : list nat :=
+Fixpoint execute s (alg : sorting_algorithm s) (xs : list nat) : list nat :=
   match alg with
-  | Done res => res
+  | Done res _ => decode_permutation res s
   | Compare n m l r => if leb (nth n xs 0) (nth m xs 0) then
                          execute r xs
                        else
                          execute l xs
   end.
 
-Definition algorithm_correct (alg : sorting_algorithm) (xs : list nat) : bool :=
+Definition algorithm_correct s (alg : sorting_algorithm s) (p : nat) : bool :=
   if list_eq_dec Nat.eq_dec
-                 (apply_permutation (execute alg xs) xs)
-                 (sort xs) then true
+                 (execute alg (decode_permutation p s))
+                 (decode_permutation p s)
+  then true
   else false.
 
-Lemma algorithm_correct_compare n m l r xs :
-  algorithm_correct (Compare n m l r) xs = true ->
-  algorithm_correct l xs || algorithm_correct r xs = true.
+Lemma algorithm_correct_compare s n m (l r : sorting_algorithm s) p :
+  algorithm_correct (Compare n m l r) p = true ->
+  algorithm_correct l p || algorithm_correct r p = true.
 Proof.
   unfold algorithm_correct. simpl.
-  destruct (leb (nth n xs 0) (nth m xs 0)).
-  - destruct (list_eq_dec Nat.eq_dec (apply_permutation (execute r xs) xs) (sort xs));
+  destruct (leb (nth n (decode_permutation p s) 0) (nth m (decode_permutation p s) 0)).
+  - destruct (list_eq_dec Nat.eq_dec
+                          (execute r (decode_permutation p s))
+                          (decode_permutation p s));
     try discriminate.
     now rewrite Bool.orb_true_r.
-  - destruct (list_eq_dec Nat.eq_dec (apply_permutation (execute l xs) xs) (sort xs));
+  - destruct (list_eq_dec Nat.eq_dec
+                          (execute l (decode_permutation p s))
+                          (decode_permutation p s));
     try discriminate.
     reflexivity.
 Qed.
@@ -403,24 +395,26 @@ Proof.
 Qed.
 
 Lemma count_impl A (f g : A -> bool) (l : list A) :
-  (forall a, f a = true -> g a = true) ->
+  (forall a, In a l -> f a = true -> g a = true) ->
   count f l <= count g l.
 Proof.
   unfold count.
   intros H.
   induction l as [|a l IH]; simpl; trivial.
+  specialize (IH (fun a' H1 H2 => H a' (or_intror H1) H2)).
   destruct (f a) eqn:E.
-  - rewrite (H _ E). simpl. omega.
+  - rewrite (H a (or_introl eq_refl) E); simpl; auto.
+    omega.
   - destruct (g a); simpl; omega.
 Qed.
 
 Lemma count_ext A (f g : A -> bool) (l : list A) :
-  (forall a, f a = g a) ->
+  (forall a, In a l -> f a = g a) ->
   count f l = count g l.
 Proof.
   intros H.
   cut (count f l <= count g l <= count f l); try solve [intuition lia].
-  split; apply count_impl; intros a; specialize (H a); congruence.
+  split; apply count_impl; intros a H'; specialize (H a H'); congruence.
 Qed.
 
 Lemma count_eq_seq : forall s l n, count (beq_nat n) (seq s l) <= 1.
@@ -450,3 +444,76 @@ Proof.
       * rewrite leb_iff_conv in *. omega.
 Qed.
 
+Lemma nth_in :
+  forall A def a (l : list A),
+    In a l ->
+    exists i, i < length l /\
+              nth i l def = a.
+Proof.
+  intros A def a l H.
+  induction l as [|a' l IH].
+  - destruct H.
+  - destruct H as [H | H].
+    + subst a'.
+      exists 0.
+      simpl. split; trivial.
+      omega.
+    + specialize (IH H).
+      destruct IH as (i & H1 & H2).
+      exists (S i). simpl. split; trivial.
+      omega.
+Qed.
+
+Lemma comp_lb_1 s (alg : sorting_algorithm s) :
+  count (algorithm_correct alg) (seq 0 (fact s)) <=
+  2 ^ (comparisons alg).
+Proof.
+  induction alg as [n m l IHl r IHr|p H].
+  - simpl.
+    rewrite (count_impl _ _ _
+                        (fun p _ => algorithm_correct_compare n m l r p)),
+            count_or.
+    rewrite (Nat.pow_le_mono_r _ _ (max (comparisons l) (comparisons r))) in IHl; try lia.
+    rewrite (Nat.pow_le_mono_r _ _ (max (comparisons l) (comparisons r))) in IHr; try lia.
+  - unfold algorithm_correct. simpl.
+    rewrite (@count_ext _ _ (beq_nat p)).
+    + apply count_eq_seq.
+    + intros p' Hp'.
+      destruct (beq_nat p p') eqn:E1,
+               (list_eq_dec Nat.eq_dec
+                            (decode_permutation p s)
+                            (decode_permutation p' s)) as [E2 | NE]; trivial.
+      * rewrite beq_nat_true_iff in E1. subst p'.
+        congruence.
+      * rewrite beq_nat_false_iff in E1. contradict E1.
+        rewrite <- (@encode_decode p s H).
+        rewrite <- (@encode_decode p' s); try congruence.
+        apply (nth_in 0) in Hp'.
+        destruct Hp' as (i & H1 & H2).
+        rewrite seq_length in H1.
+        rewrite <- H2, seq_nth; trivial.
+Qed.
+
+Lemma count_true A (l : list A) : count (fun _ => true) l = length l.
+Proof.
+  induction l as [|? l IH]; trivial.
+  unfold count in *. simpl.
+  now rewrite IH.
+Qed.
+
+Lemma comp_lb_2 s (alg : sorting_algorithm s) :
+  forallb (algorithm_correct alg) (seq 0 (fact s)) = true ->
+  s * log2 s / 2 <= comparisons alg.
+Proof.
+  intros H.
+  rewrite log2_fact.
+  rewrite <- (Nat.log2_pow2 (log2 (fact s))); try omega.
+  rewrite <- (Nat.log2_pow2 (comparisons alg)); try omega.
+  apply Nat.log2_le_mono.
+  rewrite <- comp_lb_1.
+  rewrite (@count_ext _ _ (fun _ => true)).
+  - rewrite count_true, seq_length.
+    pose proof (log2_spec (fact s) (lt_O_fact s)).
+    omega.
+  - now rewrite forallb_forall in H.
+Qed.
