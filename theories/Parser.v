45a0002320ad569f096103cf9a6fd907ea5d89dd
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-10-10 16:00:37 -0400

    Decouple reading from result extraction.

--- theories/Parser.v
+++ theories/Parser.v
@@ -1,56 +1,26 @@
 Inductive parser_data := ParserData {
   state : Type;
-  initial_state : state;
   token : Type;
-  result : Type;
-  next : state -> token -> option state;
-  partial_result : option state -> Type;
-  initial_partial_result : partial_result (Some initial_state);
-  get_result : partial_result None -> result;
-  build_result : forall s t, partial_result (Some s) -> partial_result (next s t)
+  result : state -> Type;
+  initial_state : state;
+  next : state -> token -> state;
+  initial_result : result initial_state;
+  build_result : forall s t, result s -> result (next s t)
 }.
 
 Section Parser.
 
 Variable pd : parser_data.
 
-CoInductive parser' : option (state pd) -> Type :=
-| PRead (s : state pd) (read : forall t, parser' (next pd s t)) : parser' (Some s)
-| PDone (res : result pd) : parser' None.
+CoInductive parser (s : state pd) : Type := Parser {
+  read :> forall t, parser (next pd s t);
+  get_result : result pd s
+}.
 
-Definition caseD (p : parser' None) : result pd :=
-  match p with
-  | PDone res => res
-  end.
+CoFixpoint reader' s (r : result pd s) : parser s :=
+  Parser _ (fun t => reader' _ (build_result pd s t r)) r.
 
-Definition caseR (s : state pd) (p : parser' (Some s)) : forall t, parser' (next pd s t) :=
-  match p with
-  | PRead _ read => read
-  end.
-
-Inductive parser s := Parser (p : parser' (Some s)).
-
-Definition read s (p : parser s) :=
-  fun t =>
-    match next pd s t as s' return parser' s' ->
-                                   match s' with
-                                   | Some s' => parser s'
-                                   | None => result pd
-                                   end
-    with
-    | Some s' => fun p => Parser _ p
-    | None => caseD
-    end match p with Parser p => caseR s p t end.
-
-Coercion read : parser >-> Funclass.
-
-CoFixpoint reader' s : partial_result pd s -> parser' s :=
-  match s with
-  | Some s => fun res => PRead s (fun t => reader' _ (build_result pd s t res))
-  | None   => fun res => PDone (get_result pd res)
-  end.
-
-Definition reader := Parser _ (reader' _ (initial_partial_result pd)).
+Definition reader := Parser _ (reader' _ (initial_result pd)) (initial_result pd).
 
 End Parser.
 
@@ -67,55 +37,47 @@ Notation "!-" := (Minus) (at level 0).
 Notation "!*" := (Times) (at level 0).
 Notation "! n" := (Const n) (at level 0).
 
-Definition exp_result := nat.
-
-Definition exp_next :=
-  (fun (s : nat) (t : exp_token) =>
-     match t with
-     | Plus | Minus | Times => Some (S s)
-     | Const _ => match s with
-                  | 0 => None
-                  | S s => Some s
-                  end
-     end).
-
-Fixpoint exp_partial_result' (n : nat) : Type :=
+Fixpoint exp_result' (n : nat) : Type :=
   match n with
   | 0 => nat
-  | S n' => nat -> exp_partial_result' n'
+  | S n => nat -> exp_result' n
   end.
 
-Definition exp_partial_result (s : option exp_state) : Type :=
-  match s with
-  | Some n => nat -> exp_partial_result' n
-  | None => nat
+Definition exp_result (n : exp_state) : Type :=
+  match n with
+  | 0 => unit
+  | S n => exp_result' n
   end.
 
-Definition exp_get_result (r : exp_partial_result None) : exp_result := r.
+Definition exp_next (n : exp_state) (t : exp_token) : exp_state :=
+  match n with
+  | S (S n') => match t with
+                | Plus | Minus | Times => S n
+                | Const _ => S n'
+                end
+  | _ => 0
+  end.
 
-Definition exp_build_result s t : exp_partial_result (Some s) -> exp_partial_result (exp_next s t) :=
-  match t with
-  | Plus => fun res n1 n2 => res (n1 + n2)
-  | Minus => fun res n1 n2 => res (n1 - n2)
-  | Times => fun res n1 n2 => res (n1 * n2)
-  | Const n => match s return exp_partial_result (Some s) ->
-                              exp_partial_result (exp_next s (Const n))
-               with
-               | 0 => fun res => res n
-               | S _ => fun res => res n
-               end
+Definition exp_build_result n t : exp_result n -> exp_result (exp_next n t) :=
+  match n with
+  | S (S n') =>
+    match t with
+    | Plus => fun res n1 n2 => res (n1 + n2)
+    | Minus => fun res n1 n2 => res (n1 - n2)
+    | Times => fun res n1 n2 => res (n1 * n2)
+    | Const n => fun res => res n
+    end
+  | _ => fun _ => tt
   end.
 
 Definition exp_parser_data := {|
   state := exp_state;
-  initial_state := 0;
+  initial_state := 2;
   token := exp_token;
   result := exp_result;
   next := exp_next;
-  partial_result := exp_partial_result;
-  initial_partial_result := fun t => t;
-  get_result := exp_get_result;
+  initial_result := fun t => t;
   build_result := exp_build_result
 |}.
 
-Definition my_exp : nat := reader exp_parser_data !+ !- !1 !2 !+ !4 !4.
+Definition my_exp : nat := get_result _ _ (reader exp_parser_data !+ !- !1 !2 !+ !4 !4).
