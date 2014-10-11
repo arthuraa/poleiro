3eab9b4d59b913f20fa02ef1b6ee11dfb468444f
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-10-11 18:42:30 -0400

    Add post-fix expressions.

--- theories/Parser.v
+++ theories/Parser.v
@@ -46,19 +46,30 @@ Definition my_list : list nat := [List.p _ 1 2 3 4 5 6 7 8 9 10 11] nil.
 
 Module Exp.
 
-Module Internal.
-
-Definition state := nat.
-
-Inductive token' :=
+Inductive op :=
 | Plus
 | Minus
 | Times
 | Const (n : nat).
 
-Definition token (n : state) : Type :=
-  match n with
-  | S (S _) => token'
+Module Exports.
+
+Notation "!+" := (Plus) (at level 0).
+Notation "!-" := (Minus) (at level 0).
+Notation "!*" := (Times) (at level 0).
+Coercion Const : nat >-> op.
+
+End Exports.
+
+Module Pre.
+
+Inductive state :=
+| Ok (n : nat)
+| Error.
+
+Definition token (s : state) : Type :=
+  match s with
+  | Ok (S _) => op
   | _ => Empty_set
   end.
 
@@ -68,25 +79,25 @@ Fixpoint result' (n : nat) : Type :=
   | S n => nat -> result' n
   end.
 
-Definition result (n : state) : Type :=
-  match n with
-  | 0 => Empty_set
-  | S n => result' n
+Definition result (s : state) : Type :=
+  match s with
+  | Ok n => result' n
+  | Error => Empty_set
   end.
 
-Definition next (n : state) : token n -> state :=
-  match n with
-  | S (S n') => fun t =>
-                  match t with
-                  | Plus | Minus | Times => S n
-                  | Const _ => S n'
-                  end
-  | _ => fun _ => 0
+Definition next (s : state) : token s -> state :=
+  match s with
+  | Ok (S n') => fun t =>
+                   match t with
+                   | Plus | Minus | Times => Ok (S (S n'))
+                   | Const _ => Ok n'
+                   end
+  | _ => fun _ => Error
   end.
 
-Definition build_result n : forall t, result n -> result (next n t) :=
-  match n with
-  | S (S n') =>
+Definition build_result s : forall t, result s -> result (next s t) :=
+  match s with
+  | Ok (S n') =>
     fun t =>
       match t with
       | Plus => fun res n1 n2 => res (n1 + n2)
@@ -97,29 +108,101 @@ Definition build_result n : forall t, result n -> result (next n t) :=
   | _ => fun t _ => t
   end.
 
-End Internal.
+End Pre.
 
-Definition p := {|
-  state := Internal.state;
-  initial_state := 2;
-  token := Internal.token;
-  result := Internal.result;
-  next := Internal.next;
+Definition pre := {|
+  state := Pre.state;
+  initial_state := Pre.Ok 1;
+  token := Pre.token;
+  result := Pre.result;
+  next := Pre.next;
   initial_result := fun t => t;
-  build_result := Internal.build_result
+  build_result := Pre.build_result
 |}.
 
-Module Exports.
+Module Post.
 
-Notation "!+" := (Internal.Plus) (at level 0).
-Notation "!-" := (Internal.Minus) (at level 0).
-Notation "!*" := (Internal.Times) (at level 0).
-Coercion Internal.Const : nat >-> Internal.token'.
+Inductive state :=
+| Empty
+| One
+| More (n : nat).
 
-End Exports.
+Definition token (s : state) : Type :=
+  match s with
+  | Empty | One => nat
+  | More _ => op
+  end.
+
+Fixpoint result' (n : nat) : Type :=
+  match n with
+  | 0 => nat
+  | S n => (result' n * nat)%type
+  end.
+
+Definition result (s : state) : Type :=
+  match s with
+  | Empty => unit
+  | One => nat
+  | More n => result' (S n)
+  end.
+
+Definition next s : token s -> state :=
+  match s with
+  | Empty => fun _ => One
+  | One => fun _ => More 0
+  | More 0 => fun t =>
+                match t with
+                | Plus | Minus | Times => One
+                | Const _ => More 1
+                end
+  | More (S n) => fun t =>
+                    match t with
+                    | Plus | Minus | Times => More n
+                    | Const _ => More (S (S n))
+                    end
+  end.
+
+Definition build_result s : forall t, result s -> result (next s t) :=
+  match s with
+  | Empty => fun t _ => t
+  | One => fun t r => (r, t)
+  | More 0 => fun t res =>
+                match res with
+                | (n1, n2) =>
+                  match t with
+                  | Plus => n1 + n2
+                  | Minus => n1 - n2
+                  | Times => n1 * n2
+                  | Const n => (n1, n2, n)
+                  end
+                end
+  | More (S n) => fun t res =>
+                    match res with
+                    | (res, n1, n2) =>
+                      match t with
+                      | Plus => (res, n1 + n2)
+                      | Minus => (res, n1 - n2)
+                      | Times => (res, n1 * n2)
+                      | Const n => (res, n1, n2, n)
+                      end
+                    end
+  end.
+
+End Post.
+
+Definition post := {|
+  state := Post.state;
+  token := Post.token;
+  result := Post.result;
+  initial_state := Post.Empty;
+  next := Post.next;
+  initial_result := tt;
+  build_result := Post.build_result
+|}.
 
 End Exp.
 
 Export Exp.Exports.
 
-Definition my_exp : nat := [Exp.p !+ !- 1 2 !+ 4 4].
+Definition my_exp : nat := [Exp.pre !+ !- 1 2 !+ 4 4].
+Definition my_exp' : nat := [Exp.post 4 4 !+ 2 1 !- !+].
