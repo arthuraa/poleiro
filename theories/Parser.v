a5bf0c44f96e2d6862e7d513f09f238ec56dd6c7
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-10-25 18:13:35 -0400

    Small tweaks, text on arithmetic expressions.

--- theories/Parser.v
+++ theories/Parser.v
@@ -117,7 +117,7 @@ elements on the list. We initialize the parser with an empty list, and
 each token that we read is an element of [X], which will be
 progressively added at the end of our list. *)
 
-Definition my_nat_list : list nat := [listp nat 0 1 2 3 4 5 6 7 8 9 10].
+Definition list_exp : list nat := [listp nat 0 1 2 3 4 5 6 7 8 9 10].
 
 (** For [listp], there is no need to use anything interesting for the
 [state] type. For more complicated parsers, however, [state] comes in
@@ -130,8 +130,8 @@ multiplication. *)
 Inductive op := Add | Sub | Mul.
 
 (** The expression parsers will read tokens that can be natural
-numbers or one of the above operations. Thus, we group those in a
-common type [exp_token]. *)
+numbers or one of the above operations. We group those in a common
+type [exp_token]. *)
 
 Inductive exp_token :=
 | Op (o : op)
@@ -143,32 +143,60 @@ Notation "''*'" := Mul (at level 0).
 
 Coercion Op : op >-> exp_token.
 Coercion Const : nat >-> exp_token.
-
-(** To compute the value of an expression, we map each operation to a
-Coq function: *)
-
+(* begin hide *)
+(* Denotation of an operation *)
 Definition ap_op (o : op) :=
   match o with
   | Add => plus
   | Sub => minus
   | Mul => mult
   end.
+(* end hide *)
+
+(** Let's consider how to deal with prefix expressions first. While
+parsing, the parser must read some number of complete expressions to
+finish its work, depending on which tokens it read so far. We will use
+this number as the parser state. Initially, the parser wants to read
+just one expression, so we will use that as the initial state. Once
+the state reaches zero, there are no more expressions to read, so we
+know that the parser is done. *)
 
 Module Pre.
 
 Definition state := nat.
+Definition initial_state : state := 1.
+
+(** Once the state reaches zero, any additional tokens should result
+in a parse error, since that would result in an ill-formed prefix
+expression. To implement this behavior, we define the type of tokens
+for that state to be [Empty_set]. Trying to feed an extra token to
+parser at that point will result in a type error, signaling that a
+parse error just occurred. If the parser still expects expressions, we
+just use [exp_token] for the token type. *)
 
 Definition token (s : state) : Type :=
   match s with
   | S _ => exp_token
-  | _ => Empty_set
+  | 0 => Empty_set
   end.
+
+(** The value built by the parser will be a continuation that expects
+[n] numbers, one for each expression that the parser still needs to
+read. *)
 
 Fixpoint result (n : nat) : Type :=
   match n with
   | 0 => nat
   | S n => nat -> result n
   end.
+
+(** We must define how the parser actually interprets the tokens it
+reads. If the parser expects [n] expressions, reading a constant will
+make this number go down by one, since a constant is a complete
+expression. If it reads an operation, on the other hand, that number
+is increased by one. If [n = 0], there is no token to be read, hence
+no next state, so we perform an empty pattern match to show that can't
+happen. *)
 
 Definition next (s : state) : token s -> state :=
   match s with
@@ -177,8 +205,14 @@ Definition next (s : state) : token s -> state :=
               | Op _ => S (S n')
               | Const _ => n'
               end
-  | _ => fun _ => 0
+  | 0 => fun t => match t with end
   end.
+
+(** How do we update the result? If we read a constant, we just feed
+it to the continuation. If we read an operation, we compose that
+operation with the continuation, which has the net effect of adding
+one argument to it. Here, [ap_op] is a function that maps each [op] to
+the corresponding Coq function. *)
 
 Definition read_token s : result s -> forall t, result (next s t) :=
   match s with
@@ -193,6 +227,9 @@ Definition read_token s : result s -> forall t, result (next s t) :=
 
 End Pre.
 
+(** We can now package our definitions as a complete parser and try it
+on some examples: *)
+
 Definition pre := {|
   state := Pre.state;
   initial_state := 1;
@@ -203,18 +240,48 @@ Definition pre := {|
   read_token := Pre.read_token
 |}.
 
+Definition pre_exp1 : nat :=
+  [pre '+ '- 1 2 '+ 4 4].
+Definition pre_exp2 : nat :=
+  [pre '+ '* 12 '* 12 12 '* 1 '* 1 1].
+
+(** We can also see that invalid expressions are rejected, as expected. *)
+
+Fail Definition pre_exp_wrong : nat :=
+  [pre '+ 1 1 1].
+
+(**
+<<
+Error: The term "1" has type "nat" while it is expected to have type
+"token pre (next pre (next pre (next pre (initial_state pre) '+) 1) 1)".
+>>
+
+A parser for postfix expressions is in some sense the _dual_ of the
+one we've just seen: instead of carrying around a continuation that
+expects arguments, we construct a stack of numbers on which we can
+perform our operations. Our state will also be a number, denoting the
+number of elements on our stack. Since our stack start empty, the
+initial state will be zero. *)
+
 Module Post.
 
-Inductive state :=
-| Empty
-| One
-| More (n : nat).
+Definition state := nat.
+Definition initial_state : state := 0.
+
+(** Since our operations need at least two numbers on the stack, we
+restrict our parser to accept only numbers if the stack doesn't have
+the appropriate size: *)
 
 Definition token (s : state) : Type :=
   match s with
-  | Empty | One => nat
-  | More _ => exp_token
+  | 0 | 1 => nat
+  | _ => exp_token
   end.
+
+(** The [result] type is a length-indexed list of natural numbers with
+one small twist: we ensure that [result 1 = nat] definitionally , so
+that we can use postfix expressions as having type [nat] without the
+need for any projections. *)
 
 Fixpoint result' (n : nat) : Type :=
   match n with
@@ -224,60 +291,61 @@ Fixpoint result' (n : nat) : Type :=
 
 Definition result (s : state) : Type :=
   match s with
-  | Empty => unit
-  | One => nat
-  | More n => result' (S n)
+  | 0 => unit
+  | S n => result' n
   end.
+
+(** [next] and [read_token] are dual to the definitions of the
+previous parser: reading a constant increases the stack size by one,
+while reading an operation decreases the stack size by one. *)
 
 Definition next s : token s -> state :=
   match s with
-  | Empty => fun _ => One
-  | One => fun _ => More 0
-  | More 0 => fun t =>
-                match t with
-                | Op _ => One
-                | Const _ => More 1
-                end
-  | More (S n) => fun t =>
-                    match t with
-                    | Op _ => More n
-                    | Const _ => More (S (S n))
-                    end
+  | 0 => fun _ => 1
+  | 1 => fun _ => 2
+  | S (S n) => fun t =>
+                 match t with
+                 | Op _ => 1 + n
+                 | Const _ => 3 + n
+                 end
   end.
 
 Definition read_token s : result s -> forall t, result (next s t) :=
   match s with
-  | Empty => fun _ t => t
-  | One => fun res t => (res, t)
-  | More 0 => fun res t =>
-                match res with
-                | (n1, n2) =>
-                  match t with
-                  | Op o => ap_op o n1 n2
-                  | Const n => (n1, n2, n)
-                  end
-                end
-  | More (S n) => fun res t =>
-                    match res with
-                    | (res, n1, n2) =>
-                      match t with
-                      | Op o => (res, ap_op o n1 n2)
-                      | Const n => (res, n1, n2, n)
-                      end
-                    end
+  | 0 => fun _ t => t
+  | 1 => fun res t => (res, t)
+  | 2 => fun res t =>
+           match res with
+           | (n1, n2) =>
+             match t with
+             | Op o => ap_op o n1 n2
+             | Const n => (n1, n2, n)
+             end
+           end
+  | S (S (S n)) => fun res t =>
+                     match res with
+                     | (res, n1, n2) =>
+                       match t with
+                       | Op o => (res, ap_op o n1 n2)
+                       | Const n => (res, n1, n2, n)
+                       end
+                     end
   end.
 
 End Post.
 
+(** We now have a full parser for postfix expressions, which we can
+test on some examples. *)
+
 Definition post := {|
   state := Post.state;
+  initial_state := 0;
   token := Post.token;
-  result := Post.result;
-  initial_state := Post.Empty;
   next := Post.next;
+  result := Post.result;
   initial_result := tt;
   read_token := Post.read_token
 |}.
 
-Definition my_exp : nat := [pre '+ '- 1 2 '+ 4 4].
-Definition my_exp' : nat := [post 4 4 '+ 2 1 '- '+].
+Definition post_exp1 : nat := [post 4 4 '+ 2 1 '- '+].
+Definition post_exp2 : nat := [post 9 9 '* 9 '* 10 10 '* 10 '* '+].
