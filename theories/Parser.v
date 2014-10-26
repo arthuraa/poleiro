fb02ce884ec2bea3095392f01626e2bb9749650d
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-10-26 16:07:19 -0400

    Last tweaks to text.

--- theories/Parser.v
+++ theories/Parser.v
@@ -9,11 +9,11 @@ defined outside of the core language. We will see how to implement
 some interesting syntax extensions in Coq using just coercions and
 regular Coq functions. Besides being first-class, we will see that the
 mechanism can be used for defining extensions that cannot be expressed
-with Coq's extensible parser alone.
+just with Coq's extensible parser.
 
 We want to describe each syntax extension with a few Coq types and
-functions that determine how it should be parsed. Let's start with a
-simple version of what this could look like: *)
+functions that determine how it should be parsed. Our first attempt
+might look like this: *)
 
 Module Simple.
 
@@ -24,13 +24,11 @@ Record parser := Parser {
   read_token : result -> token -> result
 }.
 
-(** Here, our parser works by reading tokens (of type [token]), one at
-a time. It analyzes those tokens to produce a final result (of type
-[result]), that it builds progressively with each new token read
-(using function [read_token]).
-
-Given such data, we can simulate a parsing process ourselves by doing
-successive calls to [read_token]. *)
+(** Our parser works by reading [token]s and returning a value of type
+[result] at the end of the process. It does this by starting with some
+value [initial_result] and updating that value with each token read,
+using function [read_token]. For instance, here's a function that
+returns the result of parsing three tokens: *)
 
 Section WithParser.
 
@@ -43,11 +41,11 @@ Definition read_three_tokens t1 t2 t3 :=
     ) t2
   ) t3.
 
-(** Needless to say, this is not very convenient as a way of extending
-Coq's syntax. To make it better, we can declare [read_token] as a
-coercion from [result] to [Funclass]. Then, we can read a stream of
-tokens simply by applying [initial_result p] to each token
-successively: *)
+(** We can use this definition as a syntax extension by declaring
+[read_token] as a coercion from [result] to [Funclass], allowing us to
+use each [result] as if it were a function. Then, applying
+[initial_result p] to a sequence of tokens will correspond to calling
+our parser on that sequence. *)
 
 Coercion read_token : result >-> Funclass.
 
@@ -64,9 +62,9 @@ is behaving as expected: *)
 Lemma read_three_tokens_same : read_three_tokens = read_three_tokens'.
 Proof. reflexivity. Qed.
 
-(** To make the mechanism more robust, we can wrap our parser in a new
+(** To make the mechanism more robust, we wrap our parser in a new
 type, ensuring that the Coq type checker will not fail to perform some
-coercions by reducing [result] more than it should. *)
+coercion by reducing [result] more than it should. *)
 
 Record parser_wrapper : Type := ParserWrapper {
   get_result : result p
@@ -79,8 +77,8 @@ Coercion read_token' : parser_wrapper >-> Funclass.
 End WithParser.
 
 (** As a last tweak, we declare another coercion and a notation to
-make using our embedded parsers more similar to usual quotations, like
-in Template Haskell: *)
+make using our embedded parsers more similar to other systems, like in
+Template Haskell: *)
 
 Definition init_parser p := ParserWrapper _ (initial_result p).
 Coercion init_parser : parser >-> parser_wrapper.
@@ -92,9 +90,8 @@ Notation "[ x ]" := (get_result _ x) (at level 0).
 Definition read_three_tokens'' (p : parser) t1 t2 t3 :=
   [p t1 t2 t3].
 
-(** As a first test for our parsing infrastructure, we can define an
-alternative syntax for Coq lists that doesn't need separators between
-the elements. *)
+(** As a first example, we can define an alternative syntax for Coq
+lists that doesn't need separators between the elements. *)
 
 Definition listp (X : Type) := {|
   token := X;
@@ -112,12 +109,13 @@ Definition list_exp : list nat := [listp nat 0 1 2 3 4 5 6 7 8 9 10].
 
 End Simple.
 
-(** While nice, [listp] is not especially interesting as a parser,
-since it always interprets the tokens it reads the same way. It also
-never produces a parse error. In contrast, parsers usually read tokens
-in a non-uniform way, treating them differently depending on what it
-has read so far. To take such dependencies into account, we introduce
-some new fields to our record: *)
+(** ** More parsers
+
+While nice, [listp] is not especially interesting as a parser, since
+it doesn't really analyze the tokens it reads. In contrast, parsers
+usually treat tokens differently, depending on what it has been read
+so far. To take such dependencies into account, we introduce some new
+fields to our record: *)
 
 Module State.
 
@@ -135,11 +133,11 @@ End State.
 
 (** The [state] field represents the internal state of our parser at a
 given point. It is set initially set to [initial_state], and is
-updated using function [next]. Additionally, [read_token] now takes
-the current state as an argument.
+updated using function [next]. We also change [read_token] to pass it
+the current state as an additional argument.
 
-While more general, this version still has some problems. We require
-our parsers to carry around a complete result value that it can return
+While more general, this version isn't quite good yet. We require our
+parsers to carry around a complete result value that it can return
 after having read _any_ sequence of tokens. Usually, however, parsing
 can result in errors, and there is no meaningful value that can be
 returned by the parser until it finishes its job. To solve this
@@ -174,15 +172,16 @@ Definition init_parser p := ParserWrapper _ _ (initial_partial_result p).
 Coercion init_parser : parser >-> parser_wrapper.
 Notation "[ x ]" := (get_partial_result _ _ x) (at level 0).
 (* end hide *)
+
 (** Now, the type of tokens expected by the parser, as well as its
 type of partial results, can depend on the current parsing state, and
 the parsing functions have been updated to take this dependency into
 account.
 
-With dependent types, the type of the value that is being built,
+With dependent types, the type of the value being built,
 [partial_result], can change during the parsing process, allowing us
 to distinguish a complete, successfully parsed result, from one that
-still needs more tokens, or even a message describing a parse
+still needs more tokens, or even from a message describing a parse
 error. By making [token] depend on the current state, we can constrain
 which tokens can be read at each parsing state, allowing us to expose
 parse errors as type errors.
@@ -190,16 +189,17 @@ parse errors as type errors.
 For [listp], there is no need to use anything interesting for the
 [state] type. For more complicated parsers, however, [state] comes in
 handy. To see how, we will define parsers for prefix and postfix
-arithmetic expressions.
+arithmetic expressions. This is also a nice example because prefix and
+postfix expressions cannot be handled by Coq's extensible parser
+alone.
 
 Expressions can involve three operations: addition, subtraction and
 multiplication. *)
 
 Inductive op := Add | Sub | Mul.
 
-(** The expression parsers will read tokens that can be natural
-numbers or one of the above operations. We group those in a common
-type [exp_token]. *)
+(** The parsers will read tokens that can be natural numbers or one of
+the above operations. We group those in a common type [exp_token]. *)
 
 Inductive exp_token :=
 | Op (o : op)
@@ -221,26 +221,26 @@ Definition ap_op (o : op) :=
   end.
 (* end hide *)
 
-(** Let's consider how to deal with prefix expressions first. While
-parsing, the parser must read some number of complete expressions to
-finish its work, depending on which tokens it read so far. We will use
-this number as the parser state. Initially, the parser wants to read
-just one expression, so we will use that as the initial state. Once
-the state reaches zero, there are no more expressions to read, so we
-know that the parser is done. *)
+(** Let's consider how to deal with prefix expressions first. At any
+point during parsing, the parser is waiting for some number of
+additional expressions it must read in order to complete the top-level
+expression. We will use this number as the parser state. Initially,
+the parser wants to read just one expression, so we will use that as
+the initial state. Once the state reaches zero, there are no more
+expressions to read, so we know that the parser is done. *)
 
 Module Pre.
 
 Definition state := nat.
 Definition initial_state : state := 1.
 
-(** Once the state reaches zero, any additional tokens should result
-in a parse error, since that would result in an ill-formed prefix
-expression. To implement this behavior, we define the type of tokens
-for that state to be [Empty_set]. Trying to feed an extra token to
-parser at that point will result in a type error, signaling that a
-parse error just occurred. If the parser still expects expressions, we
-just use [exp_token] for the token type. *)
+(** If our current state is zero, any additional tokens fed to the
+parser should result in a parse error. To implement this behavior, we
+define the type of tokens for that state to be [Empty_set]. Trying to
+feed an extra token to parser at that point will result in a type
+error, signaling that a parse error just occurred. If the parser still
+expects expressions (i.e., if the current state is greater than zero),
+we just use [exp_token] for the token type. *)
 
 Definition token (s : state) : Type :=
   match s with
@@ -317,12 +317,11 @@ Definition pre_exp2 : nat :=
 
 Fail Definition pre_exp_wrong : nat :=
   [pre '+ 1 1 1].
-
 (**
-<<
+[[
 Error: The term "1" has type "nat" while it is expected to have type
 "token pre (next pre (next pre (next pre (initial_state pre) '+) 1) 1)".
->>
+]]
 
 A parser for postfix expressions is in some sense the _dual_ of the
 one we've just seen: instead of carrying around a continuation that
@@ -417,3 +416,17 @@ Definition post := {|
 
 Definition post_exp1 : nat := [post 4 4 '+ 2 1 '- '+].
 Definition post_exp2 : nat := [post 9 9 '* 9 '* 10 10 '* 10 '* '+].
+
+(** ** Summary
+
+We've seen how we can use coercions to extend the syntax of Coq. While
+useful, this technique is _complementary_ to Coq's own extensible
+parser, having its own limitations. Despite being very programmable,
+there are also syntax extensions that cannot be defined with this
+technique in a seamless way, such as anything involving variable
+binding. Being first class can also be a disadvantage, since the
+parser and parsing functions are part of the resulting term, possibly
+affecting conversion and unification. Finally, regular syntax
+extensions are defined with a high-level declarative syntax, while
+this mechanism requires one to write a custom parser for each
+extension. *)
